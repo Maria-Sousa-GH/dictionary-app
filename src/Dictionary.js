078925ecd05bb53a53de936fe6348133cf0251cf
Maria-Sousa-GH <157738574+Maria-Sousa-GH@users.noreply.github.com>
2024-06-05 15:31:15 +0100

    imported axios and apiurl

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let key = "facba5oe04e0d0e2497c5fe825bta3c4";
+    let word = "sunset";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key={key}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
-    
   }
 
   return (
